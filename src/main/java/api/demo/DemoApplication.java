98370055c1232bfbbe8acad745ed7e7d03d49a16
Kirill Bolotin <dempi2016@yandex.ru>
2023-07-25 16:26:36 +0300

    added Swagger to project

--- src/main/java/api/demo/DemoApplication.java
+++ src/main/java/api/demo/DemoApplication.java
@@ -3,10 +3,6 @@ package api.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class DemoApplication {
